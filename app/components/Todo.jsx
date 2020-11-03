7440dacf1098a8c141538ddf9d7f1f0b2aa5cc1c
vineethaska <vineethaska@gmail.com>
2020-11-03 11:24:02 +0800

    app

---
+++ app/components/Todo.jsx
@@ -0,0 +1,10 @@
+import React, { Component, PropTypes } from 'react'
+export default class Todo extends Component {
+   render() {
+      return (
+         <li>
+            {this.props.text}
+         </li>
+      )
+   }
+}
---
+++ app/webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+
+var config = {
+    entry: './main.js',
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js',
+    },
+    devServer: {
+        inline: true,
+        port: 4000
+    },
+
+    module: {
+        rules: [{
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader','css-loader']
+            },
+            {
+                test: /\.html$/i,
+                use: ['html-loader']
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.json5$/i,
+                loader: 'json5-loader'
+            }
+
+        ],
+
+
+    }
+}
+module.exports = config;
